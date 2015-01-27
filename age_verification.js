140cc7196cf6b40f5b3d32f861155f54fe6235f7
Kate <kbalducci@hotmail.com>
2015-01-26 20:59:34 -0600

    added fizzbuzz js

---
+++ age_verification.js
@@ -0,0 +1,15 @@
+function age() {
+  console.log("How old are you?");
+}
+
+def age
+  puts "How old are you?"
+  age = gets.chomp
+end
+
+
+if age.to_i < 21
+  puts "Sorry, you are not old enough."
+else
+  puts "Great, you're old enough. What would you like to drink?"
+end
